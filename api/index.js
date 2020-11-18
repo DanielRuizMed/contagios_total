dee8cde87b8af1b08188685502039ac1f8cdf88b
DanielRuizMed <drm19933@gmail.com>
2020-11-18 18:33:47 +0100

    arreglando salida

--- api/index.js
+++ api/index.js
@@ -2,23 +2,17 @@ const datos = require("./datos.json")
 
 //funcion handler
 module.exports = (req, res) => {
-	//const { name = 'WORDL' } = req.body
-	//res.status(200).send(`Hello ${name} `)
 
 	if( Object.keys(req.query).length == 1 ){
 		
-		result = datos.filter(it => it.ccaa === req.query.ccaa )
+		result = datos.filter(it => it.ccaa === req.query.ccaa )[0]
 
-		if ( result[0] == undefined )
+		if ( result == undefined )
 			result = "Esa comunidad autónoma no existe";
 
 	}else{
 		result = "No has indicado el parametro o no es el formato indicado: /api?ccaa=Andalucía"
 	}
 
-	res.json({
-		body: result[0],
-		query: req.query,
-	})
-
+	res.status(200).json(result) 
 }
