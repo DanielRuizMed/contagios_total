013923782950e60d30d3c3386689d124c4bd08f4
DanielRuizMed <drm19933@gmail.com>
2020-11-18 12:51:35 +0100

    viendo como se pasan parametros 3

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ module.exports = (req, res) => {
 
 	var result = req.query
 
-	if( result[0] ){
+	if( result[0] != undefined ){
 		result = "Si hay parametros"
 	}else{
 		result = "No hay parametros"
