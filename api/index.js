c8188a0255b1739b13e1f870085377f43e914968
DanielRuizMed <drm19933@gmail.com>
2020-11-18 12:35:43 +0100

    viendo como se pasan parametros

--- api/index.js
+++ api/index.js
@@ -1,14 +1,15 @@
-const datos = require(datos.js)
+//const datos = require(datos.js)
 
 //funcion handler
 module.exports = (req, res) => {
 	//const { name = 'WORDL' } = req.body
 	//res.status(200).send(`Hello ${name} `)
+console.log(req.query)
 	res.json({
 		body: req.body,
 		query: req.query,
 		cookies: req.cookies,
 	})
 
-console.log(req.query)
+
 }
