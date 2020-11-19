f049bb63938b2b8634409f125e8218c2bfe50221
DanielRuizMed <drm19933@gmail.com>
2020-11-19 19:01:09 +0100

    vercel antes que shippable y test correctos 2

--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ const url= 'https://contagios-total.vercel.app/api';
 describe('Consultar número de contagios, posibles o curados ', () => {
 
     it('1.1 api?ccaa=Andalucía', (done) => {
-        chai.request(url).get('/?ccaa=Andalucía').to.have.property('ccaa').to.be.equal("Andalucía");
+        chai.request(url).get('?ccaa=Andalucía').to.have.property('ccaa').to.be.equal("Andalucía");
     });
 
 });
